987e2495f85f4498f2d996bc5cdf55fcec5092b2
Waldemar Graban <waldekgraban@gmail.com>
2020-03-06 14:21:33 +0100

    Add index.php

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ *
+ * Created by Waldemar Graban 2019
+ *
+ */
+
+namespace Waldekgraban\ClimberFallFactory;
+
+require_once "../vendor/autoload.php";
+
+use Waldekgraban\ClimberFallFactory\Downfall\Downfall;
+use Waldekgraban\ClimberFallFactory\FallFactory\FallFactory;
+use Waldekgraban\ClimberFallFactory\Rope\Rope;
+
+$climberFallsHeight = 4;
+$ropeLength         = 10;
+
+$fall        = new Downfall($climberFallsHeight);
+$rope        = new Rope($ropeLength);
+$fallFactory = new FallFactory($fall->getFallsHeight(), $rope->getRopeLength());
+
+// echo $fallFactory->getResult();      //show result
